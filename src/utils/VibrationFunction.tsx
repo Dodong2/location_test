46190ce19107286bdad5811811cab0b8e118067f
Dodong2 <carlseighartaliodegmail.com>
2024-07-15 20:46:06 +0800

    second commit

--- src/utils/VibrationFunction.tsx
+++ src/utils/VibrationFunction.tsx
@@ -1,18 +1,8 @@
-// //sample Vibration function
-// export const handleVibrate = () => {
-//     if (navigator.vibrate) {
-//         navigator.vibrate(1000)
-//     } else {
-//         console.log('Vibration API is not supported by this device.')
-//     }
-// }
+//sample Vibration function
 export const handleVibrate = () => {
-    console.log('handleVibration called');
     if (navigator.vibrate) {
-      // Vibrate for 200ms
-      navigator.vibrate(200);
-      console.log('Vibration triggered');
+        navigator.vibrate(200)
     } else {
-      console.log('Vibration API is not supported by this device.');
+        console.log('Vibration API is not supported by this device.')
     }
-  };
+}
